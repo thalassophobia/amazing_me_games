7c89489e4a8fa3fd1582785686a0ed377a90dd68
Ph3nyclidine <p.claudepeon@gmail.com>
2017-03-02 11:52:28 -0500

    Added ShapesGame.js to seperate the game scripts from the executing html

---
+++ ShapesGame.js
@@ -0,0 +1,18 @@
+var type = "WebGL"
+    if (!PIXI.utils.isWebGLSupported()) {
+      type = "canvas"
+    }
+
+    PIXI.utils.sayHello(type)
+
+      //Create the renderer
+  var renderer = PIXI.autoDetectRenderer(1024, 1024);
+
+  //Add the canvas to the HTML document
+  document.body.appendChild(renderer.view);
+
+  //Create a container object called the `stage`
+  var stage = new PIXI.Container();
+
+  //Tell the `renderer` to `render` the `stage`
+  renderer.render(stage);

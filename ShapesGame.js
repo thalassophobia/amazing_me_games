7292eb8168a8fb9c59937e89eacd1bd68d5d2724
Ph3nyclidine <p.claudepeon@gmail.com>
2017-03-02 11:56:11 -0500

    Fixed indentation in ShapesGame.js

--- ShapesGame.js
+++ ShapesGame.js
@@ -1,18 +1,18 @@
 var type = "WebGL"
-    if (!PIXI.utils.isWebGLSupported()) {
-      type = "canvas"
-    }
+if (!PIXI.utils.isWebGLSupported()) {
+    type = "canvas"
+}
 
-    PIXI.utils.sayHello(type)
+PIXI.utils.sayHello(type)
 
-      //Create the renderer
-  var renderer = PIXI.autoDetectRenderer(1024, 1024);
+//Create the renderer
+var renderer = PIXI.autoDetectRenderer(1024, 1024);
 
-  //Add the canvas to the HTML document
-  document.body.appendChild(renderer.view);
+//Add the canvas to the HTML document
+document.body.appendChild(renderer.view);
 
-  //Create a container object called the `stage`
-  var stage = new PIXI.Container();
+//Create a container object called the `stage`
+var stage = new PIXI.Container();
 
-  //Tell the `renderer` to `render` the `stage`
-  renderer.render(stage);
+//Tell the `renderer` to `render` the `stage`
+renderer.render(stage);

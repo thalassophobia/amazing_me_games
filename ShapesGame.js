2cad88aa7d5aa048c675654d5c30d04886a99467
Ph3nyclidine <p.claudepeon@gmail.com>
2017-03-02 16:24:46 -0500

    Added a draggable blue square.

--- ShapesGame.js
+++ ShapesGame.js
@@ -1,18 +1,64 @@
-var type = "WebGL"
-if (!PIXI.utils.isWebGLSupported()) {
-    type = "canvas"
+var app = new PIXI.Application(800, 600, { antialias: true });
+document.body.appendChild(app.view);
+
+app.stage.interactive = true;
+
+var square = new PIXI.Graphics();
+var rectWidth = 100;
+var rectHeight = 100
+
+//blue lines 
+square.lineStyle(2, 0x0000FF, 1);
+square.beginFill(0x0000FF, 1);
+square.drawRect(100, 100, rectWidth, rectHeight);
+square.endFill();
+
+square.interactive = true;
+square.buttonMode = true;
+
+//pointer events for touch AND mouse
+square
+    .on('pointerdown', onDragStart)
+    .on('pointerup', onDragEnd)
+    .on('pointerupoutside', onDragEnd)
+    .on('pointermove', onDragMove);
+
+square.x = 0;
+square.y = 0;
+
+app.stage.addChild(square);
+
+function onDragStart(event) {
+    // store a reference to the data
+    // the reason for this is because of multitouch
+    // we want to track the movement of this particular touch
+    this.data = event.data;
+    this.alpha = 0.5;
+    this.dragging = true;
 }
 
-PIXI.utils.sayHello(type)
+function onDragEnd() {
+    this.alpha = 1;
+    this.dragging = false;
+    // set the interaction data to null
+    this.data = null;
+}
 
-//Create the renderer
-var renderer = PIXI.autoDetectRenderer(1024, 1024);
+function onDragMove() {
+    if (this.dragging) {
+        var newPosition = this.data.getLocalPosition(this.parent);
+        this.x = newPosition.x - rectWidth * 1.5;
+        this.y = newPosition.y - rectHeight * 1.5;
+    }
+}
 
-//Add the canvas to the HTML document
-document.body.appendChild(renderer.view);
+animate();
 
-//Create a container object called the `stage`
-var stage = new PIXI.Container();
-
-//Tell the `renderer` to `render` the `stage`
-renderer.render(stage);
+function animate() {
+    requestAnimationFrame( animate );
+}
+// draw a circle
+// graphics.lineStyle(0);
+// graphics.beginFill(0xFFFF0B, 0.5);
+// graphics.drawCircle(470, 200,100);
+// graphics.endFill();

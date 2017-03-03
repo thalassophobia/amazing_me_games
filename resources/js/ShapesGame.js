398c5c700a601891d64740aa95cad8fa9386c1c0
Ph3nyclidine <p.claudepeon@gmail.com>
2017-03-02 21:12:46 -0500

    Added a draggable circle

--- resources/js/ShapesGame.js
+++ resources/js/ShapesGame.js
@@ -4,14 +4,16 @@ document.body.appendChild(app.view);
 app.stage.interactive = true;
 
 var square = new PIXI.Graphics();
+var circle = new PIXI.Graphics();
 var rectWidth = 100;
-var rectHeight = 100
+var rectHeight = 100;
+var circleRadius = 50;
 
 //Draws a dragable square
 function drawSquare(x, y) {
     square.lineStyle(2, 0x0000FF, 1);
     square.beginFill(0x0000FF, 1);
-    square.drawRect(100, 100, rectWidth, rectHeight);
+    square.drawRect(x, y, rectWidth, rectHeight);
     square.endFill();
 
     square.interactive = true;
@@ -22,14 +24,38 @@ function drawSquare(x, y) {
         .on('pointerdown', onDragStart)
         .on('pointerup', onDragEnd)
         .on('pointerupoutside', onDragEnd)
-        .on('pointermove', onDragMove);
+        .on('pointermove', onRectDragMove);
 
-    square.x = x;
-    square.y = y;
+    // square.x = x;
+    // square.y = y;
 }
-drawSquare(0, 0);
+
+//Draws a dragable circle
+function drawCircle(x, y) {
+    circle.lineStyle(0);
+    circle.beginFill(0xFF0000, 1);
+    circle.drawCircle(x, y, circleRadius);
+    circle.endFill();
+
+    circle.interactive = true;
+    circle.buttonMode = true;
+
+    //pointer events for touch AND mouse
+    circle
+        .on('pointerdown', onDragStart)
+        .on('pointerup', onDragEnd)
+        .on('pointerupoutside', onDragEnd)
+        .on('pointermove', onCircleDragMove);
+
+    // circle.x = x;
+    // circle.y = y;
+}
+
+drawCircle(150, 300);
+drawSquare(100, 100);
 
 app.stage.addChild(square);
+app.stage.addChild(circle)
 
 function onDragStart(event) {
     // store a reference to the data
@@ -47,11 +73,19 @@ function onDragEnd() {
     this.data = null;
 }
 
-function onDragMove() {
+function onRectDragMove() {
     if (this.dragging) {
         var newPosition = this.data.getLocalPosition(this.parent);
         this.x = newPosition.x - rectWidth * 1.5;
         this.y = newPosition.y - rectHeight * 1.5;
+    }
+}
+
+function onCircleDragMove() {
+    if (this.dragging) {
+        var newPosition = this.data.getLocalPosition(this.parent);
+        this.x = newPosition.x - circleRadius * 3;
+        this.y = newPosition.y - circleRadius * 6;
     }
 }
 
@@ -60,8 +94,3 @@ animate();
 function animate() {
     requestAnimationFrame(animate);
 }
-// draw a circle
-// graphics.lineStyle(0);
-// graphics.beginFill(0xFFFF0B, 0.5);
-// graphics.drawCircle(470, 200,100);
-// graphics.endFill();
